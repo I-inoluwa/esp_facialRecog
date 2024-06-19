7e155ad8cf1bd3290439bd47c4622f806ee1b337
Gabrielshow <adefelashogbanmu@gmail.com>
2024-06-19 13:16:07 +0100

    checking

--- src/server.py
+++ src/server.py
@@ -7,6 +7,23 @@ UPLOAD_FOLDER = 'uploads'
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+def detect_faces(image):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+    return faces
+
+def predict():
+    image = cv2.imread(r'./uploads/uploaded_image.jpg')
+    faces = detect_faces(image)
+    
+    for (x, y, w, h) in faces:
+        cv2.rectangle(image, (x, y), (x+w, y+h), (255, 0, 0), 2)
+    cv2.imshow('Image', image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    
 @app.route('/upload', methods=['POST'])
 def upload_file():
     if 'file' not in request.files and request.data:
